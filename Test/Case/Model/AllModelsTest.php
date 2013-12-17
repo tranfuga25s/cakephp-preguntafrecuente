de62287accc7070895fa4b76ed87cc36d64253e2
tranfuga25s <tranfuga25s@gmail.com>
2013-12-17 16:45:37 -0300

    Mejoras para pruebas

---
+++ Test/Case/Model/AllModelsTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Clase para ejecutar todos los test de modelos
+ */
+class AllModelTests extends PHPUnit_Framework_TestSuite {
+
+    /**
+     * Suite define the tests for this suite
+     *
+     * @return $suite
+     */
+    public static function suite() {
+        $suite = new PHPUnit_Framework_TestSuite('All Model Tests');
+        $path = APP_DIR . DS . 'Plugin'. DS . 'PreguntaFrecuente' . DS . 'Test' . DS . 'Case' . DS . 'Model' . DS;
+        $suite->addTestFile( $path.'CategoriaTest.php' );
+        $suite->addTestFile( $path.'PreguntaTest.php' );
+        return $suite;
+    }
+}
